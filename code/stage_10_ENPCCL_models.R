c17248f493f735498ec0f0953eb6c3cb54f55c1b
Bastián González-Bustamante <b.a.gonzalez.bustamante@fgga.leidenuniv.nl>
2025-12-11 17:18:24 +0100

    Create stage_10_ENPCCL_models.R

---
+++ code/stage_10_ENPCCL_models.R
@@ -0,0 +1,171 @@
+###############################################################################
+## SCRIPT ID:
+## Estudio análisis de uso del tiempo y relación con perfiles de participación cultural
+## November and December 2025
+## R version 4.5.0 (2025-04-11) -- "How About a Twenty-Six"
+
+## Author: Bastián González-Bustamante
+## Email: bastian.gonzalezbustamante@empirialab.cl
+## Website: https://empirialab.cl
+###############################################################################
+
+###############################################################################
+## 1. Clean environment
+###############################################################################
+
+## Clean environment
+rm(list = ls())
+
+###############################################################################
+## 2. Packages
+###############################################################################
+
+library(dplyr)
+library(pins)
+library(lmtest)
+library(plm)
+library(stargazer)
+library(fmsb)
+library(performance)
+library(brms)
+library(sjPlot)
+
+###############################################################################
+## 3. Import profiles
+###############################################################################
+
+## Aggregated estimates
+df1 <- read.csv("data/tidy/enpccl_profiles_NSE.csv")
+df2 <- read.csv("data/tidy/enpccl_profiles_grupos_etarios.csv")
+df3 <- read.csv("data/tidy/enpccl_profiles_nivel_educ.csv")
+df4 <- read.csv("data/tidy/enpccl_profiles_sexo.csv")
+
+## Sociodemographic flag
+df1$subsample <- "NSE"
+df2$subsample <- "Age"
+df3$subsample <- "Edu"
+df4$subsample <- "Sex"
+
+## Merge
+merged_df <- bind_rows(df1, df2, df3, df4)
+
+## Reference category
+merged_df <- merged_df %>%
+  mutate(
+    profile_label = relevel(factor(profile_label), ref = "Profile_6")
+  )
+
+###############################################################################
+## 4. OLS models
+###############################################################################
+
+## Naive and FEs models
+ols_1 <- lm(mean_times ~ profile_label + subsample, data = subset(merged_df, time_set == "teatro"))
+ols_2 <- lm(mean_times ~ profile_label + subsample, data = subset(merged_df, time_set == "danza"))
+ols_3 <- lm(mean_times ~ profile_label + subsample, data = subset(merged_df, time_set == "musica"))
+ols_4 <- lm(mean_times ~ profile_label + subsample, data = subset(merged_df, time_set == "cine"))
+ols_5 <- lm(mean_times ~ profile_label + subsample, data = subset(merged_df, time_set == "artesania"))
+ols_6 <- lm(mean_times ~ profile_label + subsample, data = subset(merged_df, time_set == "arte"))
+ols_7 <- lm(mean_times ~ profile_label + subsample, data = subset(merged_df, time_set == "ppoo"))
+ols_8 <- lm(mean_times ~ profile_label + subsample, data = subset(merged_df, time_set == "religion"))
+
+## Save models
+pin_write(board_folder("results/models"), ols_1, name = "ENPCCL_ols_1", versioned = TRUE)
+pin_write(board_folder("results/models"), ols_2, name = "ENPCCL_ols_2", versioned = TRUE)
+pin_write(board_folder("results/models"), ols_3, name = "ENPCCL_ols_3", versioned = TRUE)
+pin_write(board_folder("results/models"), ols_4, name = "ENPCCL_ols_4", versioned = TRUE)
+pin_write(board_folder("results/models"), ols_5, name = "ENPCCL_ols_5", versioned = TRUE)
+pin_write(board_folder("results/models"), ols_6, name = "ENPCCL_ols_6", versioned = TRUE)
+pin_write(board_folder("results/models"), ols_7, name = "ENPCCL_ols_7", versioned = TRUE)
+pin_write(board_folder("results/models"), ols_8, name = "ENPCCL_ols_8", versioned = TRUE)
+
+###############################################################################
+## 5. Breusch-Pagan test
+###############################################################################
+
+## Breusch-Pagan test for heteroscedasticity (it's not in the table)
+## p-value 95% suggests robust SEs
+bptest(ols_1) ## Robust SEs are NOT necessary
+bptest(ols_2) ## Robust SEs are necessary
+bptest(ols_3) ## Robust SEs are NOT necessary
+bptest(ols_4) ## Robust SEs are NOT necessary
+bptest(ols_5) ## Robust SEs are NOT necessary
+bptest(ols_6) ## Robust SEs are NOT necessary
+bptest(ols_7) ## Robust SEs are NOT necessary
+bptest(ols_8) ## Robust SEs are NOT necessary
+## I am going to use H1 anyway because of the size of the sample and \hat{y}
+
+###############################################################################
+## 6. Generate models table
+###############################################################################
+
+## Reestimating p-values
+pval_list <- list(
+  coeftest(ols_1, vcov = vcovHC(ols_1, type = "HC1"))[, 4],
+  coeftest(ols_2, vcov = vcovHC(ols_2, type = "HC1"))[, 4],
+  coeftest(ols_3, vcov = vcovHC(ols_3, type = "HC1"))[, 4],
+  coeftest(ols_4, vcov = vcovHC(ols_4, type = "HC1"))[, 4],
+  coeftest(ols_5, vcov = vcovHC(ols_5, type = "HC1"))[, 4],
+  coeftest(ols_6, vcov = vcovHC(ols_6, type = "HC1"))[, 4],
+  coeftest(ols_7, vcov = vcovHC(ols_7, type = "HC1"))[, 4],
+  coeftest(ols_8, vcov = vcovHC(ols_8, type = "HC1"))[, 4]
+)
+
+## LaTeX table
+stargazer(
+  ols_1, ols_2, ols_3, ols_4, ols_5, ols_6, ols_7, ols_8,
+  type            = "latex",
+  header          = FALSE,
+  style           = "ajps",
+  out             = "results/tables/ENPCCL_OLS_models.tex",
+  title           = "Efecto de los perfiles en el uso del tiempo en actividades culturales",
+  model.names     = FALSE,
+  dep.var.labels  = c("Horas de actividad"),
+  se = list(
+    coeftest(ols_1, vcov = vcovHC(ols_1, type = "HC1"))[, 2],
+    coeftest(ols_2, vcov = vcovHC(ols_2, type = "HC1"))[, 2],
+    coeftest(ols_3, vcov = vcovHC(ols_3, type = "HC1"))[, 2],
+    coeftest(ols_4, vcov = vcovHC(ols_4, type = "HC1"))[, 2],
+    coeftest(ols_5, vcov = vcovHC(ols_5, type = "HC1"))[, 2],
+    coeftest(ols_6, vcov = vcovHC(ols_6, type = "HC1"))[, 2],
+    coeftest(ols_7, vcov = vcovHC(ols_7, type = "HC1"))[, 2],
+    coeftest(ols_8, vcov = vcovHC(ols_8, type = "HC1"))[, 2]
+  ),
+  p = pval_list,
+  notes.align     = "c",
+  model.numbers   = FALSE,
+  omit.stat       = c("f", "ser"),
+  column.labels   = c("Teatro", "Danza", "Música", "Cine", "Artesanía", "Arte", "PPOO", "Religión"),
+  omit            = c("subsample"),
+  add.lines = list(
+    c("Errores robustos", "Sí", "Sí", "Sí", "Sí", "Sí", "Sí", "Sí", "Sí"),
+    c("Sociodemográficos", "Sí", "Sí", "Sí", "Sí", "Sí", "Sí", "Sí", "Sí"),
+    c("VIF",
+      format(round(as.numeric(VIF(ols_1)), 3), nsmall = 3),
+      format(round(as.numeric(VIF(ols_2)), 3), nsmall = 3),
+      format(round(as.numeric(VIF(ols_3)), 3), nsmall = 3),
+      format(round(as.numeric(VIF(ols_4)), 3), nsmall = 3),
+      format(round(as.numeric(VIF(ols_5)), 3), nsmall = 3),
+      format(round(as.numeric(VIF(ols_6)), 3), nsmall = 3),
+      format(round(as.numeric(VIF(ols_7)), 3), nsmall = 3),
+      format(round(as.numeric(VIF(ols_8)), 3), nsmall = 3)
+    ),
+    c("AIC",
+      format(round(as.numeric(model_performance(ols_1)[1]), 3), nsmall = 3),
+      format(round(as.numeric(model_performance(ols_2)[1]), 3), nsmall = 3),
+      format(round(as.numeric(model_performance(ols_3)[1]), 3), nsmall = 3),
+      format(round(as.numeric(model_performance(ols_4)[1]), 3), nsmall = 3),
+      format(round(as.numeric(model_performance(ols_5)[1]), 3), nsmall = 3),
+      format(round(as.numeric(model_performance(ols_6)[1]), 3), nsmall = 3),
+      format(round(as.numeric(model_performance(ols_7)[1]), 3), nsmall = 3),
+      format(round(as.numeric(model_performance(ols_8)[1]), 3), nsmall = 3)
+    )
+  ),
+  covariate.labels = c(
+    "Perfil 1",
+    "Perfil 2",
+    "Perfil 3",
+    "Perfil 4",
+    "Perfil 5"
+  )
+)
